3e414f0ce0d9e4b2aefe5b1580d348f6241338a7
Biswajit Sardar <biswajitsardar04082005@gmail.com>
2026-01-22 20:45:38 +0530

    Implement mobile navigation toggle and enhance mobile menu styles

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,9 @@ const menuIcon = document.querySelector(".menu-icon");
 const navList = document.querySelector(".nav-list");
 
 menuIcon.addEventListener("click", () => {
-    navList.style.display =
-        navList.style.display === "block" ? "none" : "block";
+    navList.classList.toggle("show");
 });
+
 
 // Search form validation
 const searchBtn = document.querySelector(".Search_btn");
